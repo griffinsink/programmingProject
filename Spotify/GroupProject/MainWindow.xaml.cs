ec4361d91a805931b22f5682bc9fcf543bdae158
Jack Markmiller <jackmarkmiller@ou.edu>
2019-04-22 11:48:37 -0500

    Help

---
+++ Spotify/GroupProject/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using SpotifyAPI.Web.Models;
+
+namespace GroupProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+          
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+
+            string selection;
+
+            selection = comboCategory.Text;
+
+            using (HttpClient client = new HttpClient())
+            {
+                
+
+                if (selection == "Artist")
+                {
+                    
+                    var response = client.GetAsync($"https://api.spotify.com/v1/artists/{txtboxDetails.Text}/top-tracks").Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        txtboxDetails.Clear();
+                        MessageBox.Show("You done FUCKED up.");
+                    }
+                    else if(response.IsSuccessStatusCode)
+                    {
+                        var song = response.Content.ReadAsStringAsync().Result;
+                        FullArtist a = JsonConvert.DeserializeObject<FullArtist>();
+
+                    }
+                }
+                
+            }
+        }
+    }
+}
