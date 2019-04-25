bee3e1b77dc28d0cd81d2efdbc8e91321cf8d7fa
Griffin Sink <Griffin.D.Sink@ou.edu>
2019-04-24 19:49:08 -0500

    defeated

--- Spotify/GroupProject/MainWindow.xaml.cs
+++ Spotify/GroupProject/MainWindow.xaml.cs
@@ -31,10 +31,51 @@ namespace GroupProject
 
     public partial class MainWindow : Window
     {
+        //private static SpotifyWebAPI _spotify;
         public MainWindow()
         {
             InitializeComponent();
+            //_spotify = new SpotifyWebAPI()
+            //{
+            //    UseAuth = false,
+            //};
+            //FullTrack track = _spotify.GetTrack("da3e0a9c9ef44b03b0867f5fcef7c155");
+            //Console.WriteLine(track.Name);
 
+            ImplicitGrantAuth auth = new ImplicitGrantAuth("da3e0a9c9ef44b03b0867f5fcef7c155", "http://localhost:4002", "http://localhost:4002", Scope.UserReadPrivate);
+            auth.AuthReceived += (sender, playload) =>
+            {
+                auth.Stop();
+                SpotifyWebAPI api = new SpotifyWebAPI() { TokenType = playload.TokenType, AccessToken = playload.AccessToken };
+
+            };
+
+            auth.Start();
+            auth.OpenBrowser();
+
+            //CredentialsAuth auth = new CredentialsAuth("da3e0a9c9ef44b03b0867f5fcef7c155", "8cf189c167384e71abda96d728bf305f");
+            //Token token = await auth.GetToken();
+            //SpotifyWebAPI api = new SpotifyWebAPI() { TokenType = token.TokenType, AccessToken = token.AccessToken };
+
+
+            //TokenSwapWebAPIFactory webAPIFactory;
+            //SpotifyWebAPI spotify;
+
+            //webAPIFactory = new TokenSwapWebAPIFactory("INSERT LINK TO YOUR index.php HERE")
+            //{
+            //    Scope = Scope.UserReadPrivate | Scope.UserReadEmail | Scope.PlaylistModifyPrivate,
+            //    AutoRefresh = true
+
+            //};
+
+            //try
+            //{
+            //    spotify = webAPIFactory.GetWebApiAsync().Result;
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -53,14 +94,6 @@ namespace GroupProject
             
         }
 
-        private static SpotifyWebAPI spotify;
-        public static void Main(string[] args)
-        {
-            spotify = new SpotifyWebAPI()
-            {
-                UseAuth = false,
-            }  FullTr          
-        }
 
         private void btnSelect_Click(object sender, RoutedEventArgs e)
         {
